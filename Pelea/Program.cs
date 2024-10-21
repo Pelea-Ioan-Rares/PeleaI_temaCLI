36524f081f314fb89ca8d9f9766b2348a81bde89
Pelea-Ioan-Rares <ioan.pelea@student.usv.ro>
2024-10-21 21:42:27 +0300

    Add project files.

---
+++ Pelea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pelea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Immediate window = new Immediate())
+            {
+                window.Run(60.0);  // Rulează aplicația la 60 FPS
+            }
+        }
+    }
+}
